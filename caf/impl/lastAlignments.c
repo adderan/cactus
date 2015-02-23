fcbf3e497e95e3103fbf8eeb89606cf5d2f0a85e
Alden <adderan@ucsc.edu>
2015-02-23 15:06:11 -0800

    remove binary file

--- caf/impl/lastAlignments.c
+++ caf/impl/lastAlignments.c
@@ -27,12 +27,12 @@ stList *stCaf_selfAlignFlower(Flower *flower, int64_t minimumSequenceLength, con
          */
         char *command = NULL;
         if(realign) {
-			command = stString_print("cactus_lastdb temp %s && cactus_lastal %s temp %s | maftocigar | cactus_realign %s %s", 
+			command = stString_print("cactus_lastdb temp %s && cactus_lastal %s temp %s | maftocigar --notrivial | cactus_realign %s %s", 
 				tempFile1, lastArgs, tempFile1, realignArgs, tempFile1);
         }
         else {
 			command = stString_print(
-				"cactus_lastdb temp %s && cactus_lastal %s temp %s | maftocigar", tempFile1, lastArgs, tempFile1);
+				"cactus_lastdb temp %s && cactus_lastal %s temp %s | maftocigar --notrivial", tempFile1, lastArgs, tempFile1);
         }
         FILE *fileHandle = popen(command, "r");
         if (fileHandle == NULL) {
