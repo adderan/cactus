c882caf9ad441b2d6e170e4eac72a84b56667fca
Alden <adderan@ucsc.edu>
2015-02-23 14:18:00 -0800

    add --notrivial option to maftocigar

--- scripts/maftocigar.c
+++ scripts/maftocigar.c
@@ -2,14 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int isTrivial(CigarAlignment *c)
+{
+	if(c->querystart == c->targetstart && c->querystop == c->targetstop) return 1;
+	else return 0;
+}
 int main(int argc, char **argv)
 {
-	//PairwiseMafBlock *block;
-	//block = parseMaf(stdin);
-	//pairwiseMafBlockDestruct(block);
+	int notrivial = 0;
 	PairwiseMafBlock *block;
+	if(argc > 2 && strcmp(argv[1], "--notrivial") == 0) {
+		notrivial = 1;
+		fprintf(stderr, "using --notrivial.\n");
+	}
 	while((block = parseMaf(stdin)) != NULL) {
 		CigarAlignment *ca = mafToCigar(block);
+		if(notrivial && isTrivial(ca)) continue;
 		printCigar(ca);
 		cigarAlignmentDestruct(ca);
 		pairwiseMafBlockDestruct(block);
